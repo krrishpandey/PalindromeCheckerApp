25997c298af7ac773d9018b2f2d719ecfcdb6096
KRRISH PANDEY <krrishpandeysir@gmail.com>
2026-03-10 10:35:44 +0530

    uc9

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,72 +1,28 @@
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class PalindromeChecker {
 
-    // Function to check palindrome
-    public static boolean isPalindrome(Node head) {
+    // Recursive function to check palindrome
+    public static boolean isPalindrome(String text, int start, int end) {
 
-        if (head == null || head.next == null) {
+        // Base condition
+        if (start >= end) {
             return true;
         }
 
-        // Find middle using fast and slow pointers
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        // Compare characters
+        if (text.charAt(start) != text.charAt(end)) {
+            return false;
         }
 
-        // Reverse second half of the list
-        Node prev = null;
-        Node current = slow;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-
-        // Compare first half and reversed second half
-        Node firstHalf = head;
-        Node secondHalf = prev;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
+        // Recursive call
+        return isPalindrome(text, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
 
-        String text = "radar";
+        String text = "madam";
 
-        // Convert string to linked list
-        Node head = new Node(text.charAt(0));
-        Node current = head;
-
-        for (int i = 1; i < text.length(); i++) {
-            current.next = new Node(text.charAt(i));
-            current = current.next;
-        }
-
-        // Check palindrome
-        if (isPalindrome(head)) {
+        // Call recursive function
+        if (isPalindrome(text, 0, text.length() - 1)) {
             System.out.println("The string \"" + text + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + text + "\" is NOT a Palindrome.");
